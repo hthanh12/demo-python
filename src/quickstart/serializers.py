bd14463f447dbfd35373244f9acfd07f3a977ecd
Thanh Nguyen Hoang <your_email@example.com>
2022-08-16 11:21:26 +0700

    Update setting postgres

---
+++ src/quickstart/serializers.py
@@ -0,0 +1,14 @@
+from dataclasses import field
+from pyexpat import model
+from django.contrib.auth.models import User,Group
+from rest_framework import serializers
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url','name']
